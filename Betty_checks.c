845e097b96491425489e906ef750114661f2d555
Opiyoe <opiyoelishaa@gmail.com>
2023-09-13 01:48:08 -0700

    betty is happy

---
+++ Betty_checks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: The main function that prints "Betty is Happy!"
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("Betty is Happy!\n");
+	return (0);
+}
