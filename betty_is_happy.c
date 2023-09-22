0cb818bfca1eb1b2c59d10ebde925fe8afaf1c9f
Opiyoe <opiyoelishaa@gmail.com>
2023-09-22 07:57:48 -0700

    update

--- betty_is_happy.c
+++ betty_is_happy.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
 	int num1, num2, sum;
 
-    /* Input the first number */
-	puts("Enter the first number:");
+	char result_message[50];
+
+	/* Input the first number */
+	write(STDOUT_FILENO, "Enter the first number:\n", 25);
 	scanf("%d", &num1);
 
-    /* Input the second number */
+	/* Input the second number */
+	write(STDOUT_FILENO, "Enter the second number:\n", 26);
+	scanf("%d", &num2);
 
-    puts("Enter the second number:");
-    scanf("%d", &num2);
+	/* Calculate the sum */
+	sum = num1 + num2;
 
-    /* Calculate the sum */
+	/* Convert the sum to a string */
 
-    sum = num1 + num2;
+	snprintf(result_message, sizeof(result_message), "The sum is: %d\n", sum);
 
-    /* Display the result */
+	/* Display the result using write */
+	write(STDOUT_FILENO, result_message, sizeof(result_message));
 
-    puts("The sum of the two numbers is:");
-
-    printf("%d\n", sum);
-
-    return 0;
+	return 0;
 }
